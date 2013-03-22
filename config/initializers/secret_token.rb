1ff50bf03e07220196437c05780a20e1ebc1a474
Charles Forcey <cforcey@historicusinc.com>
2013-03-22 21:31:48 +0100

    initial rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tempusfugit::Application.config.secret_key_base = 'd0832aec1efa4ca4423d8f259720da4882dbc071c13bb1e967878a56ee595c7dfa7a4a53ff51ee07d6d8281d28c0255cee9a3dbdf1b837814411bec2754a8224'
