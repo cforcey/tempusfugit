5fafcc7766aab046f57135e5b819f651732d5e10
Charles Forcey <cforcey@historicusinc.com>
2013-03-23 12:02:31 +0100

    cancan roles migration and model attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,22 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :roles
+  
+  # For a simple application, roles can be a constant for limited expansion
+  # in the future, such as an accountant or client role to view timesheets
+  # for their project but not enter any values
+  ROLES = %w[admin user]
+  
+  # getter and setter methods for setting roles using a bitwise operator to record multiple
+  # roles in a single integer field
+  def roles=(roles)
+    self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.inject(0, :+)
+  end
+  
+  def roles
+    ROLES.reject do |r|
+      ((roles_mask || 0) & 2**ROLES.index(r)).zero?
+    end
+  end
 end
